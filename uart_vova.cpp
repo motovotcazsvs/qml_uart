4ec22d32d479fc34f3cad2c88b49146101493bfc
motovotcazsvs <motovotcazsvs@gmail.com>
2021-04-14 16:38:21 +0300

    sad

--- uart_vova.cpp
+++ uart_vova.cpp
@@ -1,15 +1,15 @@
 #include "uart_vova.h"
+#include <QDebug>
 
 uart_vova::uart_vova(QObject *parent) : QObject(parent)
 {
-    int serial_port ;
-    char dat;
     serial_port = serialOpen("/dev/ttyUSB0", 9600);
     wiringPiSetup();
 
     if(serialDataAvail(serial_port)){
         dat = serialGetchar(serial_port);
+        qDebug() << dat;
         printf("%c", dat);
-        serialPutchar(serial_port, dat);
+        //serialPutchar(serial_port, dat);
     }
 }
